11b72eb556e0fe234cf00087fb6e68467c2057f8
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-02-26 23:22:21 +0500

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Anton Vikhlyaev on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
