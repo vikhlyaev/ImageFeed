14ecc3dde434124ca42848edd57fffd4aa148d8f
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-11 15:33:42 +0500

    Adds URLSession extensions

---
+++ ImageFeed/Extensions/URLSession + Extensions.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension URLSession {
+    func objectTask<T: Decodable>(for request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data, let response = response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200 ..< 300 ~= statusCode {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    if let object = try? decoder.decode(T.self, from: data) {
+                        completion(.success(object))
+                    }
+                } else {
+                    completion(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                completion(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                completion(.failure(NetworkError.urlSessionError))
+            }
+        })
+        return task
+    }
+}
