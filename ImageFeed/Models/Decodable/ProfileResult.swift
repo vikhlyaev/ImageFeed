630ae9f0f75b4cc22892c3f11f4ddc9668e08a64
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-16 23:38:30 +0500

    Fixes firstName

--- ImageFeed/Models/Decodable/ProfileResult.swift
+++ ImageFeed/Models/Decodable/ProfileResult.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct ProfileResult: Decodable {
     let username: String
-    let firstName: String?
+    let firstName: String
     let lastName: String?
     let bio: String?
 }
