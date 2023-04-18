b1ee8541f4804b281cf95b2fb31d6ddfa277bed2
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-18 23:25:51 +0500

    Adds init from PhotoResult model

--- ImageFeed/Models/Photo.swift
+++ ImageFeed/Models/Photo.swift
@@ -8,4 +8,14 @@ struct Photo {
     let thumbImageURL: String
     let largeImageURL: String
     let isLiked: Bool
+    
+    init(from photoResult: PhotoResult) {
+        self.id = photoResult.id
+        self.size = CGSize(width: photoResult.width, height: photoResult.height)
+        self.createdAt = photoResult.createdAt
+        self.welcomeDescription = photoResult.description
+        self.thumbImageURL = photoResult.urls.thumb
+        self.largeImageURL = photoResult.urls.full
+        self.isLiked = photoResult.likedByUser
+    }
 } 
