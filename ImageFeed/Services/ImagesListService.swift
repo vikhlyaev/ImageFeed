e6610e0995643ac14b6f166351255abc14d8cb09
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-20 13:30:31 +0500

    Adds changeLike method

--- ImageFeed/Services/ImagesListService.swift
+++ ImageFeed/Services/ImagesListService.swift
@@ -27,6 +27,19 @@ final class ImagesListService {
         return request
     }
     
+    private func prepareRequest(with photoId: String, and isLike: Bool) -> URLRequest? {
+        guard
+            let token = OAuthTokenStorage().token,
+            var urlComponents = URLComponents(string: Constants.baseURLString)
+        else { return nil }
+        urlComponents.path = "/photos/\(photoId)/like"
+        guard let url = urlComponents.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = isLike ? "DELETE" : "POST"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
     func fetchPhotosNextPage() {
         assert(Thread.isMainThread)
         let nextPage = lastLoadedPage == nil ? 1 : (lastLoadedPage ?? 0) + 1
@@ -50,6 +63,35 @@ final class ImagesListService {
             }
         }
         
+        self.task = task
+        task.resume()
+    }
+    
+    func changeLike(photoId: String, isLike: Bool, _ completion: @escaping (Result<Void, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        guard let request = prepareRequest(with: photoId, and: isLike) else { return }
+        let session = URLSession.shared
+        let task = URLSession.shared.objectTask(for: request) { [weak self] (result: Result<PhotoResult, Error>) in
+            guard let self else { return }
+            switch result {
+            case .success(let photoResult):
+                if let index = self.photos.firstIndex(where: { $0.id == photoResult.id }) {
+                    let photo = self.photos[index]
+                    let newPhoto = Photo(
+                        id: photo.id,
+                        size: photo.size,
+                        createdAt: photo.createdAt,
+                        welcomeDescription: photo.welcomeDescription,
+                        thumbImageURL: photo.thumbImageURL,
+                        largeImageURL: photo.largeImageURL,
+                        isLiked: photoResult.likedByUser)
+                    self.photos = self.photos.withReplaced(itemAt: index, newValue: newPhoto)
+                }
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
         self.task = task
         task.resume()
     }
