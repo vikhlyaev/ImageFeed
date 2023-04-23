d8316bc615e184aedc5e407aea31277b2b2c7a6f
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-23 23:43:14 +0500

    Adds Secrets to check

---
+++ ImageFeed/Secrets.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Secrets {
+    static let accessKey = "ugMqz4pOBvfxQ3OYf9Zftj95xymv9vESsYZtj3pvVwA"
+    static let secretKey = "Hf3LM5kly0LsGfVAGUPhkESuAaLYfukc_-mWXWYRwHo"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let authorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let tokenURLString = "https://unsplash.com/oauth/token"
+    static let baseURLString = "https://api.unsplash.com"
+}
