1cad7ddeceabad2318a9ec42d6b5794eca5ab613
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-24 00:00:27 +0500

    Makes ModuleBuilder singleton

--- ImageFeed/Screens/ModuleBuilder.swift
+++ ImageFeed/Screens/ModuleBuilder.swift
@@ -2,8 +2,14 @@ import UIKit
 
 final class ModuleBuilder {
     
-    static func makeWebModule(delegate: WebViewControllerDelegate) -> UIViewController {
-        let presenter = WebPresenter()
+    static let shared = ModuleBuilder()
+    
+    private let authHelper: AuthHelper = AuthHelperImpl(configuration: AuthConfiguration.standart)
+    
+    private init() {}
+    
+    func makeWebModule(delegate: WebViewControllerDelegate) -> UIViewController {
+        let presenter = WebPresenter(authHelper: authHelper)
         let vc = WebViewController(output: presenter)
         presenter.viewInput = vc
         vc.delegate = delegate
