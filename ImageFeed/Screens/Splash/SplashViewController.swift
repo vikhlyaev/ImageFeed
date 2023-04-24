bc5e27444317bd7e154a1f8b209be3fc06300c00
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-24 23:50:34 +0500

    Adds comments

--- ImageFeed/Screens/Splash/SplashViewController.swift
+++ ImageFeed/Screens/Splash/SplashViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class SplashViewController: UIViewController {
     
+    // MARK: - UI
+    
     private lazy var logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "YandexPracticum")
@@ -10,10 +12,14 @@ final class SplashViewController: UIViewController {
         return imageView
     }()
     
+    // MARK: - Services
+    
     private let oauthService = OAuthService()
     private let oauthTokenStorage = OAuthTokenStorage()
     private let profileService = ProfileService.shared
     private let profileImageService = ProfileImageService.shared
+    
+    // MARK: - Life Cycle
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -30,6 +36,8 @@ final class SplashViewController: UIViewController {
         setupView()
         setConstraints()
     }
+    
+    // MARK: - Setup UI
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
@@ -39,6 +47,8 @@ final class SplashViewController: UIViewController {
         view.backgroundColor = UIColor(named: "customBlack")
         view.addSubview(logoImageView)
     }
+    
+    // MARK: - Actions
     
     private func switchToTabBarController() {
         guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
